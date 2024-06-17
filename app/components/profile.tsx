c55fe7c1cf6b6ade45395b02de50ddd1efda2e75
Pietro Bongiovanni <pietrobongiovanni@icloud.com>
2024-06-17 12:03:07 +0200

    x

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -8,7 +8,7 @@ type ProfileProps = {
 const Profile: React.FC<ProfileProps> = () => {
     return (
         <div className="text-center">
-            <Image src="https://placeholder.com/350x150" alt="Player Avatar" width={350} height={150} />
+            <Image src="https://cards.scryfall.io/art_crop/front/e/5/e5d1c814-4c22-4917-95ae-dffaf491db32.jpg?1717644342" alt="Player Avatar" width={350} height={150} sizes="(max-width: 350px), (max-height: 150px)" />
         </div>
     )
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['placeholder.com'],
+        domains: ['placeholder.com', 'cards.scryfall.io'],
     },
 };
 
