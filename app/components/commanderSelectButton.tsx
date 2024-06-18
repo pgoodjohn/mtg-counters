1485881394fcc59889e8fcd02cff96174ffb0446
Pietro Bongiovanni <pietrobongiovanni@icloud.com>
2024-06-18 17:48:07 +0200

    x

--- app/components/commanderSelectButton.tsx
+++ app/components/commanderSelectButton.tsx
@@ -7,7 +7,7 @@ import {
     HoverCard,
     HoverCardContent,
     HoverCardTrigger,
-  } from "@/components/ui/hover-card"
+} from "@/components/ui/hover-card"
 
 type CommanderSelectButtonProps = {
     card: Card;
@@ -15,30 +15,30 @@ type CommanderSelectButtonProps = {
     handleCommanderSelected: (commander: Commander) => void;
 };
 
-const CommanderSelectButton: React.FC<CommanderSelectButtonProps> = ({card, commander, handleCommanderSelected}) => {
+const CommanderSelectButton: React.FC<CommanderSelectButtonProps> = ({ card, commander, handleCommanderSelected }) => {
 
     return (
-        <HoverCard>
-            <HoverCardTrigger>
-                <Button variant="ghost" onClick={() => handleCommanderSelected(commander)} className="h-[100px] w-full flex">
+        <Button variant="ghost" onClick={() => handleCommanderSelected(commander)} className="h-[100px] w-full flex">
+            <HoverCard>
+                <HoverCardTrigger>
                     <div className="w-[100px] mr-2">
                         {card.image_uris &&
-                        <Image src={card.image_uris.art_crop} alt={card.name} height="626" width="274" />
+                            <Image src={card.image_uris.art_crop} alt={card.name} height="626" width="274" />
                         }
                     </div>
                     <div className="flex items-center justify-center">
                         <p className="p-4">{card.name}</p>
                     </div>
                     <div className="flex-grow"></div>
-                </Button>
-            </HoverCardTrigger>
-            <HoverCardContent className="flex w-auto">
-                {card.image_uris &&
-                    <Image src={card.image_uris.large} alt={card.name} height="626" width="274" />
-                }
-                <p className="max-w-[200px] p-4">{card.oracle_text}</p>
-            </HoverCardContent>
-        </HoverCard>
+                </HoverCardTrigger>
+                <HoverCardContent className="flex w-auto">
+                    {card.image_uris &&
+                        <Image src={card.image_uris.large} alt={card.name} height="626" width="274" />
+                    }
+                    <p className="max-w-[200px] p-4">{card.oracle_text}</p>
+                </HoverCardContent>
+            </HoverCard>
+        </Button>
     )
 };
 
